80c4107216d886398ac5b90ddd27bbdac3784c29
sumona8436 <sumonasonai50@gmai.com>
2024-03-17 10:59:43 +0530

    Revert "new file added"
    This reverts commit 775136b7500f227c573dedc2becbf61c9161f75c.

--- class.js
+++
@@ -1,9 +0,0 @@
-class Animal {
-  constructor(name, legCount) {
-    this.name = name;
-    this.legCount = legCount;
-  }
-  describe() {
-    return `${this.name} has ${this.legCount} legs`;
-  }
-}
